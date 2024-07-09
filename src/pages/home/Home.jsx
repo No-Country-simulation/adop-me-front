2fc9713efe06655b8d941b839d667aafbd5e7c5f
Zail <rekcahet@gmail.com>
2024-07-08 22:58:40 -0500

    avances de home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,9 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
+import { Facebook } from 'react-bootstrap-icons';
+import { Instagram, Google, Github } from 'react-bootstrap-icons';
+
 
 export const Home = () => {
   return (
@@ -35,6 +38,15 @@ export const Home = () => {
                 <Button variant="primary" size="lg">
                   Empezar
                 </Button>{" "}
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+              <i class="bi bi-facebook"></i>
+              <Facebook size={50} color="gray" />
+              <Instagram size={50} color="#E4405F" className="mx-3" />
+        <Google size={50} color="#DB4437" className="mx-3" />
+        <Github size={50} color="black" className="mx-3" />
               </Col>
             </Row>
           </Container>
