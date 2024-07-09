f8f4deb2551fd3eabb4d60445b978ec33e2e4f9d
Zail <rekcahet@gmail.com>
2024-07-08 22:31:12 -0500

    agregar bootstrap al archivo global

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+
 
 export const Home = () => {
   return (
-    <h1>Home</h1>
+    <Container>
+    <Row>
+      <Col>1 of 2</Col>
+      <Col>2 of 2 <Button variant="primary">Primary</Button>{' '}</Col>
+    </Row>
+  </Container>
   )
 }
