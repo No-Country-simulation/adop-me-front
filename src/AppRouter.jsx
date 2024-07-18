4046b0fa24843187970de32c01e4bae7b4d15d44
Joaquin <joaquin.butera@gmail.com>
2024-07-18 00:25:57 -0300

    first changes

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/home/Home";
+import { SuccessStories } from "./pages/sucessStories/SuccessStories";
 
 export const AppRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/aboutus" element={<SuccessStories />}/>
     </Routes>
   );
 };
---
+++ src/pages/sucessStories/SuccessStories.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+export const SuccessStories = () => {
+  return <Container>
+    
+  </Container>;
+};
