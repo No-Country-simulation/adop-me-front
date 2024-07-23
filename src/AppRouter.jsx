74f884339c3ed5c6311b088ad2507b6f17b4a4fd
Martin <martin_dotto@hotmail.com>
2024-07-23 16:17:33 -0300

    Actualizacion componentes

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,25 +1,26 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/home/Home";
+import Mascotas from "./pages/mascota/Mascotas";
 import NavBar from "./components/NavBar/NavBar";
 import { Contact } from "./pages/contact/Contact";
 import { About } from "./pages/about/About";
 import Footer from "./components/Footer/Footer";
-import { Share } from "./pages/share/Share";
 import { Adopt } from "./pages/adopt/Adopt";
 
 export const AppRouter = () => {
   return (
     <>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/share" element={<Share />} />
-        <Route path="/adopt" element={<Adopt />} />
-      </Routes>
-      <Footer />
+    <NavBar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/mascotas" element={<Mascotas />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/adopt" element={<Adopt />} />
+    </Routes>
+    <Footer />
     </>
   );
 };
+    
